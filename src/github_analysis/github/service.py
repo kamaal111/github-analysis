d50f4ff973f31302c6e22eed1979807abdd87039
Kamaal Farah <kamaal.f1@gmail.com>
2024-10-06 11:01:21 +0000

    Displaying PRs as a dataframe

--- src/github_analysis/github/service.py
+++ src/github_analysis/github/service.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import polars as pl
+
 from .client.client import GitHubClient
 from .client.users import PullRequestStates
 
@@ -10,14 +12,37 @@ class GitHubService:
     def __init__(self) -> None:
         self.__client = GitHubClient()
 
+    async def get_users_pull_requests_as_data_frame(
+        self,
+        username: str,
+        until: datetime | None = None,
+        filter_states: list[PullRequestStates] = [],
+    ) -> pl.DataFrame:
+        users_pull_requests = await self.get_users_pull_requests(
+            username=username, until=until, filter_states=filter_states
+        )
+        data_frame_data = {
+            "created_at": [],
+            "amount_of_comments": [],
+            "repository_name": [],
+        }
+        for users_pull_request in users_pull_requests:
+            data_frame_data["created_at"].append(users_pull_request.createdAt)
+            data_frame_data["amount_of_comments"].append(
+                users_pull_request.comments.totalCount
+            )
+            data_frame_data["repository_name"].append(
+                users_pull_request.baseRepository.nameWithOwner
+            )
+
+        return pl.DataFrame(data_frame_data)
+
     async def get_users_pull_requests(
         self,
         username: str,
         until: datetime | None = None,
         filter_states: list[PullRequestStates] = [],
     ):
-        result = await self.__client.users.get_pull_requests(
+        return await self.__client.users.get_pull_requests(
             username=username, filter_states=filter_states, until=until
         )
-
-        return result

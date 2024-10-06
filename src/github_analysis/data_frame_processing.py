7b7b814da32a601d9815c5c56da118e24a8bbcef
Kamaal Farah <kamaal.f1@gmail.com>
2024-10-06 18:23:53 +0000

    Aggregating pull request reviews

--- src/github_analysis/data_frame_processing.py
+++ src/github_analysis/data_frame_processing.py
@@ -7,9 +7,39 @@ from .github.client.validators.pull_requests import GitHubPullRequest
 def pull_request_reviews_as_data_frame(
     pull_request_reviews: list[GitHubPullRequestContribution],
 ):
-    data_frame_data = {}
+    data_frame_data = {
+        "created_at": [],
+        "author": [],
+        "amount_of_comments": [],
+        "repository_name": [],
+        "pull_request_number": [],
+    }
+    for pull_request_review in pull_request_reviews:
+        pull_request_author = pull_request_review.pullRequest.author
+        data_frame_data["author"].append(
+            pull_request_author.login if pull_request_author is not None else None
+        )
+        data_frame_data["repository_name"].append(
+            pull_request_review.repository.nameWithOwner
+        )
+        data_frame_data["amount_of_comments"].append(
+            pull_request_review.pullRequest.comments.totalCount
+        )
+        data_frame_data["pull_request_number"].append(
+            f"#{pull_request_review.pullRequest.number}"
+        )
+        data_frame_data["created_at"].append(pull_request_review.pullRequest.createdAt)
 
-    return pl.DataFrame(data_frame_data)
+    return pl.DataFrame(data_frame_data).sort("created_at", descending=True)
+
+
+def group_pull_request_reviews_by_repositories(pull_request_reviews: pl.DataFrame):
+    return (
+        pull_request_reviews.filter(pl.col("amount_of_comments") == 0)
+        .group_by("repository_name")
+        .agg(pl.len().alias("amount_of_reviews"))
+        .sort("amount_of_reviews", descending=True)
+    )
 
 
 def pull_requests_as_data_frame(pull_requests: list[GitHubPullRequest]):
